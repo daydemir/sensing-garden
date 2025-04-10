5dfb72efb5dc1f473d32ef5ec4c5105a361192a0
Deniz Aydemir <deniz@aydemir.us>
2025-04-10 19:19:53 -0400

    download resources executable, make paths relative in run_sensing_garden_tl

--- run_sensing_garden_tl.sh
+++ run_sensing_garden_tl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Path to the Python script you want to run
-TL_PYTHON_SCRIPT="/home/bplusplus/Documents/sensing-garden/time-lapse/timelapse_video.py"
+TL_PYTHON_SCRIPT="./time-lapse/timelapse_video.py"
 
 # Log file for output and errors
-TIME_LAPSE_LOG_FILE="/home/bplusplus/Documents/sensing-garden/log-files/timelapse.log"
+TIME_LAPSE_LOG_FILE="./log-files/timelapse.log"
 
 # Source the environment
-source "/home/bplusplus/Documents/sensing-garden/setup_env.sh"
+source "./setup_env.sh"
 
 
 # Run the Python script with parsed arguments and log output/errors
