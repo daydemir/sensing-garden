977641a78e477588117ebfdc55fb34a334581c9b
aasehaa <ase.hatveit@gmail.com>
2025-04-28 15:29:31 +0200

    adding temperature check on device before running script t prevent overheating

--- run_sensing_garden_tl.sh
+++ run_sensing_garden_tl.sh
@@ -6,11 +6,20 @@ TL_PYTHON_SCRIPT="./time-lapse/timelapse_video.py"
 # Log file for output and errors
 TIME_LAPSE_LOG_FILE="./log-files/timelapse.log"
 
-# Source the environment
-source "./setup_env.sh"
-
-
-# Run the Python script with parsed arguments and log output/errors
-python3 "$TL_PYTHON_SCRIPT" >> "$TIME_LAPSE_LOG_FILE" 2>&1
-
 #sleep 540 # does not work in shells cript - ADD IN PYTHON SCRIPT
+
+# Read CPU temperature in millidegrees Celsius and convert to degrees
+cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpu_temp_c=$((cpu_temp / 1000))
+
+# Only run script if temperature is below 80 degrees Celsius to prevent overheating
+if [ "$cpu_temp_c" -lt 80 ]; then
+    # Source the environment
+    /source "./setup_env.sh"
+
+    # Run the Python script with parsed arguments and log output/errors
+    python3 "$TL_PYTHON_SCRIPT" >> "$TIME_LAPSE_LOG_FILE" 2>&1
+
+else
+    echo "CPU temperature is $cpu_temp_c°C, which is above the safe threshold. Script will not run."
+fi
